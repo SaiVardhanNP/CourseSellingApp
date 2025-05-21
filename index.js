f6f56592f5cc0f718230907472c7c682ea758210
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-05-21 12:38:13 +0530

    step-2 define routes

--- index.js
+++ index.js
@@ -4,13 +4,38 @@ const jwt=require("jsonwebtoken")
 const app=express();
 
 
+app.use(express.json());
 
-app.post("/signup",(req,res)=>{
+
+app.post("/user/signup",(req,res)=>{
+    res.json({
+        msg:"Sign up end-point!"
+    })
 
 })
 
-app.post("/signin",(req,res)=>{
+app.post("user/signin",(req,res)=>{
+    res.json({
+        msg:"Sign in end point!"
+    })
 
+})
+
+app.get("/courses",(req,res)=>{
+    res.json({
+        msg:"courses end point!"
+    })
+})
+app.get("/user/purchases",(req,res)=>{
+    res.json({
+        msg:"user purchase end point!"
+    })
+})
+
+app.post("/courses/purchase",(req,res)=>{
+    res.json({
+        msg:"purchase a course end point!"
+    })
 })
 
 
