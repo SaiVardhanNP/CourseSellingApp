c2b0e16fe06416ddd9590e96edfbe8993b9671e5
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-05-21 12:21:32 +0530

    start of project

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const jwt=require("jsonwebtoken")
+
+const app=express();
+
+
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+
+})
+
+
+
+app.listen(3000);
